216e5da7f0d91b1b702791f87ffff2c3bd250620
Ahmed Allam <ahmed.allam@abwaab.com>
2023-08-18 15:05:51 +0300

    do , while , loops

--- main.dart
+++ main.dart
@@ -1,30 +1,14 @@
 import 'dart:ffi';
 
 void main() {
-  var list = [1, 2, 44, 58, 2, 268, 54, 895];
+  var isActive = true;
 
-  // / nested for
-  for (int i = 0; i < list.length; i++) {
-    for (int j = 0; j < list.length; j++) {
-      print(j);
-      print(i);
-    }
+  while (isActive) {
+    print("Active");
+    isActive = false;
   }
-
-
-
+  
+  do {
+    print("Hello");
+  } while (isActive);
 }
-
-
-///Data Types
-/// String, int, double, bool
-/// Var , dynamic
-/// List , Map
-/// 
-/// var record = (a: 1, b: 2, 3);
-// print(record.$1);
-
-
-
-///Loops
-

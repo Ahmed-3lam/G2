1e8519d7865b59caac61f443ac82e978d620c278
Ahmed Allam <ahmed.allam@abwaab.com>
2023-08-18 16:12:12 +0300

    switch , if

--- main.dart
+++ main.dart
@@ -2,14 +2,28 @@ import 'dart:ffi';
 
 void main() {
   bool isActive = true;
-  
-  // if (isActive) {
-
+  // if (!isActive) {
+  //   print("true");
+  // } else if (1 != 0) {
+  //   print("false");
+  // } else if (1 != 1) {
   //   print("true");
   // } else {
-  //   print("false");
+  //   print("End");
   // }
 
-  // isActive ? print("true") : print("false");
+  var command = "OPEN";
+  switch (command) {
+    case "CLOSED"  :
+      print("CLOSED");
 
+    case "OPEN":
+      print("OPEN");
+    case "LOCKED":
+      print("LOCKED");
+    default:
+      print("NOT FOUND");
+  }
+
+  
 }

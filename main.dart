3d42332b863050e1ff6d62ad629f75a894615dab
Ahmed Allam <ahmed.allam@abwaab.com>
2023-08-18 14:57:41 +0300

    first commit

---
+++ main.dart
@@ -0,0 +1,30 @@
+import 'dart:ffi';
+
+void main() {
+  var list = [1, 2, 44, 58, 2, 268, 54, 895];
+
+  /// nested for
+  // for (int i = 0; i < list.length; i++) {
+  //   for (int j = 0; j < list.length; j++) {
+  //     print(j);
+  //     print(i);
+  //   }
+  // }
+
+
+  
+}
+
+
+///Data Types
+/// String, int, double, bool
+/// Var , dynamic
+/// List , Map
+/// 
+/// var record = (a: 1, b: 2, 3);
+// print(record.$1);
+
+
+
+///Loops
+

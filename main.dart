2260b14b2077dfe12f6705d8b7eb02db331fb672
Ahmed Allam <ahmed.allam@abwaab.com>
2023-08-19 09:39:44 +0300

    revision on data types

--- main.dart
+++ main.dart
@@ -1,29 +1,32 @@
-import 'dart:ffi';
-
 void main() {
+  /// [Variable type]  variable name  =  value;
+  /// String , int , double , bool, List , Map , Set , Record ... enu
+  String name = " Mohamed";
+  int price1 = 5;
+  double price2 = 5.5;
   bool isActive = true;
-  // if (!isActive) {
-  //   print("true");
-  // } else if (1 != 0) {
-  //   print("false");
-  // } else if (1 != 1) {
-  //   print("true");
-  // } else {
-  //   print("End");
-  // }
+  List<int> myList1 = [1, 2, 3, 4, 4];
+  Map<String, dynamic> myMap1 = {"id": 1, "name": "product1"};
 
-  var command = "OPEN";
-  switch (command) {
-    case "CLOSED"  :
-      print("CLOSED");
+  List<Map<String, dynamic>> myList2 = [
+    {"id": 1, "name": "product1"},
+    {"id": 1, "name": "product1"}
+  ];
 
-    case "OPEN":
-      print("OPEN");
-    case "LOCKED":
-      print("LOCKED");
-    default:
-      print("NOT FOUND");
-  }
+  Map<String, List<int>> myMap2 = {
+    "id": [1, 2, 4]
+  };
 
-  
+  Set mySet = {1, 2, 4, 2, 2};
+
+  var record = (1, 2, a: 5, b: 8);
+ 
+
+  /// [var]  variable name = value ;
+  var name2 = "ahmed";
+  name2 = "mohamed";
+
+  /// dynamic  variable name = value ;
+  dynamic p = "ahmed";
+  p = 2;
 }

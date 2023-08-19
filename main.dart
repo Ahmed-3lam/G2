8e1002ebaa8a9d9cf09cda0bca6fdf69992cba12
Ahmed Allam <ahmed.allam@abwaab.com>
2023-08-19 09:44:34 +0300

    loops

--- main.dart
+++ main.dart
@@ -1,32 +1,23 @@
 void main() {
-  /// [Variable type]  variable name  =  value;
-  /// String , int , double , bool, List , Map , Set , Record ... enu
-  String name = " Mohamed";
-  int price1 = 5;
-  double price2 = 5.5;
   bool isActive = true;
-  List<int> myList1 = [1, 2, 3, 4, 4];
-  Map<String, dynamic> myMap1 = {"id": 1, "name": "product1"};
+  int count = 0;
+  while (count < 5) {
+    count++;
+    print("Dont stop");
+  }
 
-  List<Map<String, dynamic>> myList2 = [
-    {"id": 1, "name": "product1"},
-    {"id": 1, "name": "product1"}
-  ];
+  do {
+    count++;
+    print("Dont stop");
+  } while (count < 5);
 
-  Map<String, List<int>> myMap2 = {
-    "id": [1, 2, 4]
-  };
+  for (int i = 0; i < 10; i++) {
+    print(i);
+  }
 
-  Set mySet = {1, 2, 4, 2, 2};
+  List<int> list = [1, 2, 5, 4, 8, 6, 9];
 
-  var record = (1, 2, a: 5, b: 8);
- 
-
-  /// [var]  variable name = value ;
-  var name2 = "ahmed";
-  name2 = "mohamed";
-
-  /// dynamic  variable name = value ;
-  dynamic p = "ahmed";
-  p = 2;
+  for (var item in list) {
+    print(item);
+  }
 }

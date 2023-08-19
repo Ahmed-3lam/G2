53eec759e8376455b0c71517022a370dc72ffeb1
Ahmed Allam <ahmed.allam@abwaab.com>
2023-08-19 13:52:06 +0300

    optional params

--- main.dart
+++ main.dart
@@ -1,54 +1,25 @@
 import 'dart:io';
 
-import 'const.dart';
-import 'screens/home_screen.dart';
-
 void main() {
-  printMyName("Ahmed", 15, "Bmw");
-  printMyNameWithNamedParams(name: "Ahmed", age: 15, car: "Bmw");
-  printMyNameWithNamedAndUnNamed(
-    "Ahmed",
-    "Car",
-    age: 15,
-  );
+  namedParams(street: "2514");
+  namedParams(name: "Mohamed", street: "2514");
+  namedParams(car: "Fiat", street: "2514");
 }
 
-/// Functions
-
-// [type]  [functionName]  (){},
-void printMyName(String name, int age, String car) {
-  print("My Name is $name");
-  print("My Age is $age");
-  print("My Car is $car");
-}
-
-void printMyNameWithNamedParams({
-  required String name,
-  required int age,
-  required String car,
+///Optional Params
+void namedParams({
+  String? name,
+  String? car,
+  String? address,
+  required String street,
 }) {
-  print("My Name is $name");
-  print("My Age is $age");
-  print("My Car is $car");
-}
-
-void printMyNameWithNamedAndUnNamed(
-  String name,
-  String car, {
-  required int age,
-}) {
-  print("My Name is $name");
-  print("My Age is $age");
-  print("My Car is $car");
+  if (name != null) print(name);
+  if (car != null) print(car);
+  if (address != null) print(address);
+  print(street);
 }
 
 
 
+/// Don't Repeat yourself
 
-
-/// void
-/// Return types (4 functions)
-/// unNamed Params
-/// Named Params ( void , return)
-/// Named & UnNamed Params( void , return)
-/// 
